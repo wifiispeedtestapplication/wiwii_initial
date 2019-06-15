3a18b3c05e67dda3e954a061ddb74ba9caae8560
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-06-15 14:58:11 -0400

    Added user master and detail page

---
+++ wiwii/UserMasterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace wiwii
+{
+    public partial class UserMasterPage : ContentPage
+    {
+        public UserMasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
